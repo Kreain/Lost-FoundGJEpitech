60ede1b0caf917c67d4f16fc8877d79ecc10613c
Gaetanse <gs99@hotmail.fr>
2021-01-31 15:27:26 +0100

    spawn/fin map

--- perso.h
+++ perso.h
@@ -39,16 +39,8 @@ void deplacement_perso(int valeur) {
 	sfSprite_move(sprite_perso, position);
 }
 
-void update_perso(int choix) {
-	if (choix == 0) {
-		persoY = 3;
-		persoX = 0;
-	}
-	else if (choix == 1) {
-		//persoY = 0;
-		//persoX = 0;
-	}
-	sfVector2f position = { sizePixels * persoX, sizePixels * persoY };
+void update_perso(int x,int y) {
+	sfVector2f position = { sizePixels * x, sizePixels * y };
 	sfSprite_setPosition(sprite_perso, position);
 };
 
